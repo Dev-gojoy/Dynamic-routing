d5504882e24ea4e3099b50afebeaf08665d90826
Dev-gojoy <32jongwoo@gmail.com>
2023-04-04 14:23:42 +0900

    first commit

---
+++ life/src/App.jsx
@@ -0,0 +1,13 @@
+import ClassComponent from "./components/ClassComponent";
+import FunctionComponent from "./components/FunctionComponent";
+
+function App() {
+  return (
+    <div className="bg-red-100 min-h-screen">
+      <ClassComponent />
+      <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ life/src/components/FunctionComponent.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+const FunctionComponent = () => {
+  useEffect(() => {}, []);
+
+  return <div className="bg-blue-100 h-80">FunctionComponent</div>;
+};
+
+export default FunctionComponent;
